c30f532ab311abbc5040d1d6553fb0acdfc0fda2
Nofigroup <m.diallo@nofi.fr>
2016-11-21 14:33:52 +0100

    - first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# hubsine-marketplace
---
+++ hubsine-marketplace.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * Plugin Name: Hubsine Marketplace
+ * Plugin URI: http://hubsinemarketplace.com
+ * Description: Plugin market place pour woocommerce
+ * Version: 0.1
+ * Author: Hubsine
+ * Author URI: https://hubsine.com   
+ * Licence: GPL2
+ */
+
+// Exit if accessed directly
+if( !defined( 'ABSPATH' ) ) {
+    die;
+}
+
